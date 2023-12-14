bdf3371c9c962247d5702a352f469cba651e59f1
Rishikesh K V <rishikeshkaroth@gmail.com>
2023-12-14 23:47:22 +0530

    Add e-commerce data modeling files

---
+++ e-commerce-data-modeling/models/product.models.js
@@ -0,0 +1,40 @@
+const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    productImg: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
